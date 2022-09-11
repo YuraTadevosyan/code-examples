9d42ac93fbf0c43c17b2261cbcd56d3a5bfdf17c
YuraTadevosyan <yuratadevosyan01@gmail.com>
2022-09-11 17:30:10 +0400

    Solving exercise

---
+++ JS Exercises and Code Examples/exercise 2/index.js
@@ -0,0 +1,12 @@
+// Check Readme.ms file
+
+function toCamelCase(str){
+    let arr = str.split('');
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '-' && arr[i+1] !== undefined || arr[i] === '_') {
+            arr[i] = '';
+            arr[i+1] = arr[i+1].toUpperCase();
+        }
+    }
+    return arr.join('');
+}
