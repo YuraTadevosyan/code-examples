77669ecd5193cd8999bfae6209e77a4183135e64
YuraTadevosyan <yuratadevosyan01@gmail.com>
2022-11-17 01:09:14 +0400

    Solving exercise

---
+++ JS Exercises and Code Examples/exercise 16/index.js
@@ -0,0 +1,19 @@
+// Check Readme.ms file
+
+function sumTwoSmallestNumbers(numbers) {
+    let sum = 0;
+
+    sum += getSmallestElement(numbers);
+
+    if(numbers.length > 2) {
+        let removeFfirstSmallest = numbers.splice(numbers.indexOf(sum),1);
+
+        sum += getSmallestElement(numbers)
+    }
+
+    return sum
+}
+
+function getSmallestElement(arr) {
+    return Math.min(...arr)
+}
