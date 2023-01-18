7aaf49c6f70dfd05dc14eecae8dce9b373535f3d
YuraTadevosyan <yuratadevosyan01@gmail.com>
2023-01-18 23:05:20 +0400

    Solving exercise

---
+++ JS Exercises and Code Examples/exercise 38/index.js
@@ -0,0 +1,13 @@
+// Check Readme.ms file
+function alphabet(ns) {
+    let D;
+    const CD = Math.max(...ns);
+
+    for (let i = 2; i < Math.ceil(Math.sqrt(CD)); i++) {
+        if (CD % i === 0 && ns.includes(i) && ns.includes(CD / i) && i < (CD / i)) {
+            D = CD / i;
+        }
+    }
+
+    return D;
+}
