0a7ccd0ea5b2deef41e6e4646d1c501350a55670
YuraTadevosyan <yuratadevosyan01@gmail.com>
2023-01-23 23:55:27 +0400

    Solving exercise

---
+++ JS Exercises and Code Examples/exercise 41/index.js
@@ -0,0 +1,9 @@
+// Check Readme.ms file
+function perfectSquare(string){
+    const slashN = string.split('.').filter(Boolean);
+    const dots = string.split('\n');
+
+    if (slashN.length + 1 === dots[0].length && dots.every(el => el === dots[0])) return true;
+
+    return false;
+}
