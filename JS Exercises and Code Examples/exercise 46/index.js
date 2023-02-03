d615b96b17224bb6e80ce7a4e9d72c88aa6fb22c
YuraTadevosyan <yuratadevosyan01@gmail.com>
2023-02-03 23:31:18 +0400

    Solving exercise

---
+++ JS Exercises and Code Examples/exercise 46/index.js
@@ -0,0 +1,6 @@
+// Check Readme.ms file
+const epley = (w, r) => w * (1 + r / 30)
+const mcglothin = (w, r) => 100 * w / (101.3 - 2.67123 * r)
+const lombardi = (w, r) => w * Math.pow(r, 0.1)
+
+const calculate1RM = (w, r)=> r === 1 ? w : r === 0 ? 0 : Math.round(Math.max(epley(w, r), mcglothin(w, r), lombardi(w, r)))
