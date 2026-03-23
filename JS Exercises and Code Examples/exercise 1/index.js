4c18c19685d2f71f585678a32ffe0e623ce60439
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-03-23 22:58:37 +0400

    Remove unnecessary semicolons and fix formatting

--- JS Exercises and Code Examples/exercise 1/index.js
+++ JS Exercises and Code Examples/exercise 1/index.js
@@ -1,13 +1,13 @@
 // Check Readme.md file
 
 function solution(number){
-    let sum = 0;
+    let sum = 0
     for (let i = 3; i < number; i++ ) {
         if (i % 15 === 0) {
-            sum += i;
+            sum += i
         } else if (i % 5 === 0 || i % 3 === 0) {
-            sum += i;
+            sum += i
         }
     }
-    return sum;
+    return sum
 }
