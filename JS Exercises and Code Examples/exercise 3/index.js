ea443989be1c5602ccdbf73d055ada26bfc27a5b
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-03-25 21:12:33 +0400

    Refactor descendingOrder function for clarity

--- JS Exercises and Code Examples/exercise 3/index.js
+++ JS Exercises and Code Examples/exercise 3/index.js
@@ -1,8 +1,6 @@
 // Check Readme.md file
 
-function descendingOrder(n){
-    n += '';
-    let arr = n.split('');
-    arr.sort((a,b)=>b-a);
-    return +arr.join('');
+function descendingOrder(n) {
+    n += ''
+    return +n.split('').sort((a,b)=> b - a).join('')
 }
