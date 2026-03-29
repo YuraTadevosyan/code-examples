031f88e27beafb87e2a4f36c1846ebbecff19887
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-03-29 20:26:49 +0400

    Update index.js

--- JS Exercises and Code Examples/exercise 6/index.js
+++ JS Exercises and Code Examples/exercise 6/index.js
@@ -2,16 +2,16 @@
 
 function isPrime(num) {
     if (num <= 1) {
-        return false;
+        return false
     } else if (num == 2) {
-        return true;
+        return true
     }
 
     for (let i = 2; i <= Math.ceil(Math.sqrt(num)); i++) {
         if (num % i === 0) {
-            return false;
+            return false
         }
     }
 
-    return true;
+    return true
 }
