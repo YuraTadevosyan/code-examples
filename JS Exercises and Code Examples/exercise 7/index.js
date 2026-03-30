735c5381a736b4fd2502ed5d954ac0cfaca5da5a
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-03-30 21:24:27 +0400

    Refactor spinWords function for cleaner syntax

--- JS Exercises and Code Examples/exercise 7/index.js
+++ JS Exercises and Code Examples/exercise 7/index.js
@@ -1,10 +1,10 @@
 // Check Readme.md file
 
 function spinWords(string){
-    let arr = [];
-    string.split(' ').forEach( e => {
-        if (e.length >= 5) e = e.split('').reverse().join('');
-        arr.push(e);
-    });
-    return arr.join(' ');
+    let arr = []
+    string.split(' ').forEach(e => {
+        if (e.length >= 5) e = e.split('').reverse().join('')
+        arr.push(e)
+    })
+    return arr.join(' ')
 }
