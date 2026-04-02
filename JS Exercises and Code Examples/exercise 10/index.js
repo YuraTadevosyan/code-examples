38f92a9b8dffdb348df180c1c84a03897fa7f4a4
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-04-02 23:54:43 +0400

    Refactor anagram function for clarity

--- JS Exercises and Code Examples/exercise 10/index.js
+++ JS Exercises and Code Examples/exercise 10/index.js
@@ -1,8 +1,8 @@
 // Check Readme.md file
 
 function anagrams(word, words) {
-    let sortedWord = word.split('').sort().join('');
-    const arr = [];
+    let sortedWord = word.split('').sort().join('')
+    const arr = []
 
     words.forEach(e => {
         if (e.split('').sort().join('') === sortedWord) arr.push(e)
