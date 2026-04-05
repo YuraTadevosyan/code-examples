96fac35651e55d687b31744c5b88bae845587577
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-04-05 20:10:39 +0400

    Refactor int32ToIp function for consistency

--- JS Exercises and Code Examples/exercise 14/index.js
+++ JS Exercises and Code Examples/exercise 14/index.js
@@ -1,11 +1,11 @@
 // Check Readme.md file
 
 function int32ToIp(int32){
-    let str = ``;
-    let toBinary = int32.toString(2);
+    let str = ``
+    let toBinary = int32.toString(2)
 
     for(let i = 0; i < 4; i++) {
-        let str1 = toBinary.slice(8 * i, 8 + 8 * i);
+        let str1 = toBinary.slice(8 * i, 8 + 8 * i)
 
         if(str1 == '') {
             str += `${0}.`
