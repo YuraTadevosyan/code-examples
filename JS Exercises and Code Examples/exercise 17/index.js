dd16d16b52f7ad1a434e8367ba397f3fe8fee3ee
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-04-09 22:50:17 +0400

    Refactor incrementString function for readability

--- JS Exercises and Code Examples/exercise 17/index.js
+++ JS Exercises and Code Examples/exercise 17/index.js
@@ -1,14 +1,14 @@
 // Check Readme.md file
 
 function incrementString (strng) {
-    let reversestring = reverseString(strng);
-    let number = parseInt(reversestring);
+    let reversestring = reverseString(strng)
+    let number = parseInt(reversestring)
 
-    if(isNaN(number)) return reverseString(1 + reversestring);
+    if(isNaN(number)) return reverseString(1 + reversestring)
 
     if (number == 0) return reverseString(reversestring.replace(0, 1))
 
-    let numberWithoutZeros = parseInt(reverseString(number.toString()));
+    let numberWithoutZeros = parseInt(reverseString(number.toString()))
 
     let revNum = reverseString((numberWithoutZeros + 1).toString().padStart(number.toString().length, 0))
 
