246832edd6b47e5266c56524958a5b1986dfdf05
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-04-11 20:28:47 +0400

    Refactor Roman numeral conversion methods

--- JS Exercises and Code Examples/exercise 19/index.js
+++ JS Exercises and Code Examples/exercise 19/index.js
@@ -1,11 +1,10 @@
 // Check Readme.md file
 
 const numbers = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1}
-const arabicNums = Object.values(numbers);
-const romanNums = Object.keys(numbers);
+const arabicNums = Object.values(numbers)
+const romanNums = Object.keys(numbers)
 
 class RomanNumeralsClass {
-
     toRoman(num) {
         if(arabicNums.indexOf(num) !== -1) {
             return romanNums[arabicNums.indexOf(num)]
@@ -13,10 +12,10 @@ class RomanNumeralsClass {
 
         let convertNum = ''
 
-        for (let i in numbers ) {
-            while ( num >= numbers[i] ) {
-                convertNum += i;
-                num -= numbers[i];
+        for (let i in numbers) {
+            while (num >= numbers[i]) {
+                convertNum += i
+                num -= numbers[i]
             }
         }
 
@@ -28,15 +27,15 @@ class RomanNumeralsClass {
             return arabicNums[romanNums.indexOf(num)]
         }
 
-        let convertNum = 0;
+        let convertNum = 0
 
         for (let i = 0; i < num.length; i++) {
-            const curr = numbers[num[i]];
-            const next = numbers[num[i + 1]];
-            (curr < next) ? (convertNum -= curr) : (convertNum += curr);
+            const curr = numbers[num[i]]
+            const next = numbers[num[i + 1]]
+            (curr < next) ? (convertNum -= curr) : (convertNum += curr)
         }
 
-        return convertNum;
+        return convertNum
     }
 }
 
