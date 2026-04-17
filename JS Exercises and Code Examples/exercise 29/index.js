90b58607a26b311214d144b5e80b72ea31205c51
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-04-17 22:22:23 +0400

    Remove unnecessary semicolons in index.js

--- JS Exercises and Code Examples/exercise 29/index.js
+++ JS Exercises and Code Examples/exercise 29/index.js
@@ -1,4 +1,4 @@
 // Check Readme.md file
-const min = (list) => Math.min(...list);
+const min = (list) => Math.min(...list)
 
-const max = (list) => Math.max(...list);
+const max = (list) => Math.max(...list)
