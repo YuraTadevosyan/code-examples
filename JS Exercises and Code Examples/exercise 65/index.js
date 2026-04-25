ce25fb664b4861e9b76561213161cd791715a9da
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-04-25 20:39:57 +0400

    Fix formatting of greet function in index.js

--- JS Exercises and Code Examples/exercise 65/index.js
+++ JS Exercises and Code Examples/exercise 65/index.js
@@ -1,2 +1,2 @@
 // Check Readme.md file
-const greet = (name) => `Hello ${name.toLowerCase().replace(/^./, name[0].toUpperCase())}!`;
+const greet = (name) => `Hello ${name.toLowerCase().replace(/^./, name[0].toUpperCase())}!`
