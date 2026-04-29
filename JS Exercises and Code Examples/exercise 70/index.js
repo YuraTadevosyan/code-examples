4033dc0e61f28dae91178ada90b4e252998f8557
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-04-29 15:45:40 +0400

    Update index.js

--- JS Exercises and Code Examples/exercise 70/index.js
+++ JS Exercises and Code Examples/exercise 70/index.js
@@ -1,8 +1,8 @@
 // Check Readme.md file
 const sequenceSum = (begin, end, step) => {
-  let num = 0;
+  let num = 0
 
-  for (let i = begin; i <= end; i += step) num += i;
+  for (let i = begin; i <= end; i += step) num += i
 
-  return num;
+  return num
 };
