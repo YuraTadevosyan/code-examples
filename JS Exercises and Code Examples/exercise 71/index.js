28535ce0162de7e5e7e5afaea46b738307993800
YuraTadevosyan <71385162+YuraTadevosyan@users.noreply.github.com>
2026-05-01 14:17:26 +0400

    Fix newline at end of index.js
    Removed newline at the end of the file.

--- JS Exercises and Code Examples/exercise 71/index.js
+++ JS Exercises and Code Examples/exercise 71/index.js
@@ -1,2 +1,2 @@
 // Check Readme.md file
-const findAverage = (array) => array.length ? array.reduce((a, b) => a + b, 0) / array.length : 0;
+const findAverage = (array) => array.length ? array.reduce((a, b) => a + b, 0) / array.length : 0
